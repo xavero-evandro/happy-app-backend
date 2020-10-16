165e09c88a64818650a950326c827afc96ee929f
xavero-evandro <evandromelos@hotmail.com>
2020-10-16 22:34:28 +0200

    added relations and upload image manipulation

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
-import { Router } from 'express';
+import { Router } from 'express'
 import OrphanagesController from './controllers/OrphanagesController'
+import multer from 'multer'
+import uploadConfig from './config/upload'
 
-const routes = Router();
+const routes = Router()
+const upload = multer(uploadConfig)
 
 routes.get("/orphanages", OrphanagesController.index)
-routes.post("/orphanages", OrphanagesController.create)
+routes.get("/orphanages/:id", OrphanagesController.show)
+routes.post("/orphanages", upload.array('images'), OrphanagesController.create)
 
 export default routes
